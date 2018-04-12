1c0a54ea018efb47e6a7db662a1b70bda784500d
Nikolay Konovalov <konovalov.nk@gmail.com>
2018-04-12 07:50:53 +0300

    - Fixed int -> integer.

--- db/migrate/20180412034305_create_user_tags.rb
+++ db/migrate/20180412034305_create_user_tags.rb
@@ -2,7 +2,7 @@ class CreateUserTags < ActiveRecord::Migration[5.1]
   def change
     create_table :user_tags do |t|
       t.references :user, foreign_key: true
-      t.int :start_from
+      t.integer :start_from
       t.string :rating
       t.text :tags
 
--- db/migrate/20180412034520_create_tag_pictures.rb
+++ db/migrate/20180412034520_create_tag_pictures.rb
@@ -3,7 +3,7 @@ class CreateTagPictures < ActiveRecord::Migration[5.1]
     create_table :tag_pictures do |t|
       t.references :user_tag, foreign_key: true
       t.references :picture, foreign_key: true
-      t.int :is_seen
+      t.integer :is_seen
 
       t.timestamps
     end

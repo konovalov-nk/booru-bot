bcef449b9967227e84c1ac52e145b8ee60db2387
Nikolay Konovalov <konovalov.nk@gmail.com>
2018-04-12 06:31:00 +0300

    - Changed a few things to `pictures` table

--- db/migrate/20180401054425_create_pictures.rb
+++ db/migrate/20180401054425_create_pictures.rb
@@ -1,12 +1,13 @@
 class CreatePictures < ActiveRecord::Migration[5.1]
   def change
     create_table :pictures do |t|
-      t.integer :idx
-      t.string :hash
-      t.string :rating
-      t.string :image
+      t.integer :booru_idx
       t.string :file_url
-      t.string :tags
+      t.string :hash
+      t.string :image
+      t.string :rating
+      t.string :source
+      t.text :tags
       t.integer :change
       t.integer :width
       t.integer :height
